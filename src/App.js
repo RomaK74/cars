74a854938b7d0d2c36395788e50f2cefd5e42371
RomaK74 <75043799+RomaK74@users.noreply.github.com>
2021-07-27 04:35:56 +0300

    V-01-and-F-01-fix-and-V-02

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Route, Switch} from 'react-router-dom';
+import {MainPage} from "./pages/MainPage";
+import {Order} from "./pages/Order/Order";
+import {BackgroundMenu} from "./components/BackgroundMenu";
+import {LineMenu} from "./components/LineMenu";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const background = React.useRef();
+
+    const usingBM = (iconStatus) => {
+        if (!iconStatus) {
+            document.getElementsByClassName('line-menu')[0].style.background = '#111518';
+            document.getElementsByClassName('line-menu__language')[0].style.display = 'none';
+            background.current.style.display = 'block';
+            if (document.getElementById('sliderFone'))
+                document.getElementById('sliderFone').classList.add("fone");
+        } else {
+            background.current.style.display = 'none';
+            document.getElementsByClassName('line-menu')[0].style.background = '#151B1F';
+            document.getElementsByClassName('line-menu__language')[0].style.display = 'block';
+            if (document.getElementById('sliderFone'))
+                document.getElementById('sliderFone').classList.remove("fone");
+        }
+    }
+
+    return (
+        <div>
+            <div ref={background} className="menu-background">
+                <BackgroundMenu/>
+            </div>
+            <div className="main-container">
+                <LineMenu toggleBM={usingBM}/>
+                <Switch>
+                    <Route exact path="/cars/build" render={() => <MainPage pressBM={usingBM}/>}/>
+                    <Route path="/cars/build/order" render={() => <Order pressBM={usingBM}/>}/>
+                </Switch>
+            </div>
+        </div>)
 }
 
 export default App;
