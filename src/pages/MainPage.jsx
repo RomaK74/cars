624e141f38bc920e8e377f3a4d2e9b2a90da6368
RomaK74 <75043799+RomaK74@users.noreply.github.com>
2021-07-27 04:35:03 +0300

    V-01-and-F-01-fix-and-V-02

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,19 +1,13 @@
-import React, {useState} from 'react';
-import slide1 from "../img/slide-1.png";
-import slide2 from "../img/slide-3.png";
-import slide3 from "../img/slide-2.png";
-import slide4 from "../img/slide-4.png";
+import React from 'react';
 import {BurgerMenu} from "../components/BurgerMenu";
-import location from "../img/location.png";
 import Slider from "../components/Slider";
-import {LineMenu} from "../components/LineMenu";
 import {Link} from "react-router-dom";
-import {BackgroundMenu} from "../components/BackgroundMenu";
 import {Location} from "../components/Location";
 import {Logo} from "../components/Logo";
+import {images} from './images';
+import {Button} from "../components/Button";
 
-export const MainPage = () => {
-    const images = [slide1, slide2, slide3, slide4];
+export const MainPage = ({pressBM}) => {
     const titles = ['Бесплатная парковка', 'Страховка', 'Бензин', 'Обслуживание'];
     const subtitles = ['Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.',
         'Полная страховка страховка автомобиля', 'Полный бак на любой заправке города за наш счёт', 'Автомобиль проходит еженедельное ТО'];
@@ -44,53 +38,35 @@ export const MainPage = () => {
         },
     ]
 
-    React.useEffect(() => {
-
-    }, [window.innerWidth])
-
     const usingBM = (iconStatus) => {
-        console.log('qq', iconStatus);
-        if (!iconStatus) {
-                document.getElementById('menuBack').style.display = 'block';
-                document.getElementById('sliderFone').classList.add("fone");
-                //document.getElementsByClassName('burgerMenu__firstLine')[i].style.display = 'none';
-                // document.getElementsByClassName('burgerMenu__forthLine')[i].style.display = 'none';
-        } else {
-
-                document.getElementById('menuBack').style.display = 'none';
-                document.getElementById('sliderFone').classList.remove("fone");
-        }
+        pressBM(iconStatus);
     }
 
 
     return (
         <div className="App" id="app">
-            <div className="menu-background" id="menuBack">
-                <BackgroundMenu />
-            </div>
             <div className="container">
-                <LineMenu toggleBM={usingBM} />
                 <div className="main-part">
-                    <div className="main-part-up">
+                    <header className="main-part-up">
                         <BurgerMenu className="main-part--bm" toggleBM={usingBM}/>
                         <div className="main-part-up__TL">
                             <Logo />
                             <Location />
                         </div>
-                    </div>
-                    <div className="main-part-middle">
+                    </header>
+                    <main className="main-part-middle">
                         <div className="main-part-middle__title">Каршеринг</div>
                         <div className="main-part-middle__subtitle">Need for drive</div>
                         <div className="main-part-middle__subtitle2">Поминутная аренда авто твоего города</div>
                         <div className="main-part-middle__button">
-                            <Link to="/cars/build/order"><button className="main-part-middle__button-book">Забронировать</button></Link>
+                            <Link to="/cars/build/order/location"><Button className="main-part-middle__button-book" btnText="Забронировать"/></Link>
                         </div>
-                    </div>
-                    <div className="main-part-footer">
+                    </main>
+                    <footer className="main-part-footer">
                         <div className="main-part-footer__year">© 2016-2019 «Need for drive»</div>
                         <div className="main-part-footer__phone"><a href="tel:8 (495) 234-22-44">8 (495) 234-22-44</a>
                         </div>
-                    </div>
+                    </footer>
                 </div>
                 <div className="slider-part">
                     <div className="" id="sliderFone"></div>
